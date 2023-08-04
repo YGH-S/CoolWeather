7936da903ab2686c44cd0bd56151d2a2b08c47e9
YYGH <2951774602@qq.com>
2023-08-04 09:15:37 +0800

    加入创建数据库和表的各项操作。

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,42 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+    private int id;
+    private String cityNamel;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityNamel() {
+        return cityNamel;
+    }
+
+    public void setCityNamel(String cityNamel) {
+        this.cityNamel = cityNamel;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
